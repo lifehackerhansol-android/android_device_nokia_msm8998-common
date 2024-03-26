f91be1d066cdce6f1697f176201f96acc899cb8b
Michael Bestas <mkbestas@lineageos.org>
2024-03-26 20:32:54 +0700

    msm8998-common: Build Lineage Health HAL
    Change-Id: I39ff7d80ccf86935ec07ff0203d382e527bcfff6

--- msm8998-common.mk
+++ msm8998-common.mk
@@ -236,6 +236,10 @@ PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service \
     lights.msm8998
 
+# Lineage Health
+PRODUCT_PACKAGES += \
+    vendor.lineage.health-service.default
+
 # Low-Power whitelist
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml
